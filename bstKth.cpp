dda5c62a0f4971dda909c23a41965af4836dc172
Collin Eades <cjeades@tesla.cs.iupui.edu>
2018-10-01 16:44:32 -0400

    latest commit wihtout U input

--- bstKth.cpp
+++ bstKth.cpp
@@ -1,9 +1,10 @@
 //this program finds the kth smallest element in a binary seach tree
 #include<iostream>
+#include<queue>
 using namespace std;
 
 
-//build tree
+//binary tree nodes
 struct BstNode {
 	int data;
 	BstNode* left;
@@ -39,9 +40,10 @@ BstNode* Insert(BstNode* root, int data){
 	return root;
 }
 
-int PrintInOrder(BstNode* root, int kth, int counter){
+//function for In Order traversal (to calculate k)
+int InOrder(BstNode* root, int kth, int counter){
 
-	//if there have been 15 node datas printed, return smallest element
+	//if there have been 15 node's data  printed, return smallest element
 	//THIS COULD EASILY BE CHANGED TO BE DYNAMIC TO MORE THAN 15 
 	if (counter==15){
 		return kth;
@@ -52,13 +54,10 @@ int PrintInOrder(BstNode* root, int kth, int counter){
 		if (root->left != NULL){
 
 			//go to next node to the left 
-			PrintInOrder(root->left, kth, counter);
+			InOrder(root->left, kth, counter);
 		}
 
-		//print the nodes data
-		cout<<root->data<<" ";
-
-		//snag smallest when currently on it
+		//if the current parse number  == declared k, set that data to k
 		if (counter==kth){
 
 			kth=root->data;
@@ -70,7 +69,7 @@ int PrintInOrder(BstNode* root, int kth, int counter){
 		if (root->right !=NULL){
 
 			//go to next node to the right
-			PrintInOrder(root->right, kth, counter);
+			InOrder(root->right, kth, counter);
 		}
 	}else{
 
@@ -78,8 +77,78 @@ int PrintInOrder(BstNode* root, int kth, int counter){
 	}
 }
 
+//function for printing level order traversal of the tree
+void printLevelOrder(BstNode* root){
+
+	if (root == NULL) return;
+
+	//create queue
+	queue<BstNode* >q;
+
+	//enqueue root and initialize height
+	q.push (root);
+
+	while (1){
+
+		//set nodeCount to size of queue/number of nodes
+		int nodeCount = q.size();
+		if (nodeCount == 0){
+
+			break;
+		}
+
+		//pop out current level and push next level of tree
+		while (nodeCount > 0){
+
+			BstNode* node = q.front();
+			cout<<node->data<< " ";
+			q.pop();
+			if (node->left != NULL){
+				q.push(node->left);
+			}
+			if (node-> right != NULL){
+
+				q.push(node->right);
+			}
+			nodeCount--;
+
+		}
+		cout<<endl;
+	}
+	/*queue<BstNode*> q;
+	q.push(root);
+	q.push(NULL);
+	while (!q.empty()){
+
+		BstNode *n=q.front();
+		q.pop();
+		if (n!=NULL){
+
+			if(n->left!=NULL){
+				q.push(n->left);
+			}
+			if(n->right!=NULL){
+				q.push(n->right);
+			}
+			cout<<n->data<<" ";
+		}
+		else
+		{
+			cout<<"\n";
+			if(!q.empty()){
+				q.push(NULL);
+			}
+		}
+	}*/
+
+
+
+}
+
 int main(){
 
+	//initialzie array
+	int array[15] = {6, 17, 20, 41, 45, 52, 57, 65, 71, 76, 79, 87, 92, 95, 99};
 
 	//variable for inputted array keys and K variables
 	int dataPassed, k;
@@ -93,28 +162,33 @@ int main(){
 	//promt for value of k
 	cout<<"\nHello. Please enter a value for K: ";
 	cin>>k;
-
-	//prompt user for data
-	cout<<"\nPlease enter (in order) each number in the array given for the assignemnt: ";
 	
 	//data is inputted into tree
 	for (int i=1; i<=15; i++){
 
-		cout<<"\nelement "<<i<<" : ";
-		cin>>dataPassed;
+		dataPassed=array[i];
 		root = Insert(root, dataPassed);
 	}
 
+	cout<<"\nThank you.  Printing tree based off assignment array...  \n\n";
+
 	//print tree
-	cout<<"\nThank you.  Printing tree...  ";
+	printLevelOrder(root);
 
 
-	//call print function for tree, pass K
-	k=PrintInOrder(root, k, counter);
+	//find K
+	k=InOrder(root, k, counter);
 
 	//print kth smallest
-	cout<<"\nThe Kth smallest element is: "<<k;
+	cout<<"\nThe Kth smallest element is: "<<k<<"\n";
+
+	return 0;
 
 }
 
 
+/*
+ *
+ * PRINT IN THIS ORDER: ROOT, LEFT CHILD, RIGHT CHILD.  A B C B D E C F G
+ *
+ */
