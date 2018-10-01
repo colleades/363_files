34abbced161c25df2f4879f1fd9b2ec0572e2d1a
colleades <40572507+colleades@users.noreply.github.com>
2018-10-01 19:23:37 -0400

    Update bstKthPractice.cpp

--- bstKthPractice.cpp
+++ bstKthPractice.cpp
@@ -1,66 +1,53 @@
-// Binary Search Tree - Implemenation in C++
-// Simple program to create a BST of integers and search an element in it 
+/* Binary tree - Level Order Traversal */
 #include<iostream>
+#include<queue>
 using namespace std;
-//Definition of Node for Binary search tree
-struct BstNode {
-	int data; 
-	BstNode* left;
-	BstNode* right;
+
+struct Node {
+	int data;
+	Node *left;
+	Node *right;
 };
 
-// Function to create a new Node in heap
-BstNode* GetNewNode(int data) {
-	BstNode* newNode = new BstNode();
-	newNode->data = data;
-	newNode->left = newNode->right = NULL;
-	return newNode;
+// Function to print Nodes in a binary tree in Level order
+void LevelOrder(Node *root) {
+	if(root == NULL) return;
+	queue<Node*> Q;
+	Q.push(root);  
+	//while there is at least one discovered node
+	while(!Q.empty()) {
+		Node* current = Q.front();
+		Q.pop(); // removing the element at front
+		cout<<current->data<<" ";
+		if(current->left != NULL) Q.push(current->left);
+		if(current->right != NULL) Q.push(current->right);
+	}
 }
-
-// To insert data in BST, returns address of root node 
-BstNode* Insert(BstNode* root,int data) {
-	if(root == NULL) { // empty tree
-		root = GetNewNode(data);
+// Function to Insert Node in a Binary Search Tree
+Node* Insert(Node *root,int data) {
+	if(root == NULL) {
+		root = new Node();
+		root->data = data;
+		root->left = root->right = NULL;
 	}
-	// if data to be inserted is lesser, insert in left subtree. 
-	else if(data <= root->data) {
-		root->left = Insert(root->left,data);
-	}
-	// else, insert in right subtree. 
-	else {
-		root->right = Insert(root->right,data);
-	}
+	else if(data <= root->data) root->left = Insert(root->left,data);
+	else root->right = Insert(root->right,data);
 	return root;
 }
-//To search an element in BST, returns true if element is found
-bool Search(BstNode* root,int data) {
-	if(root == NULL) {
-		return false;
-	}
-	else if(root->data == data) {
-		return true;
-	}
-	else if(data <= root->data) {
-		return Search(root->left,data);
-	}
-	else {
-		return Search(root->right,data);
-	}
-}
+
 int main() {
-	BstNode* root = NULL;  // Creating an empty tree
-	/*Code to test the logic*/
-	root = Insert(root,15);	
-	root = Insert(root,10);	
-	root = Insert(root,20);
-	root = Insert(root,25);
-	root = Insert(root,8);
-	root = Insert(root,12);
-	// Ask user to enter a number.  
-	int number;
-	cout<<"Enter number be searched\n";
-	cin>>number;
-	//If number is found, print "FOUND"
-	if(Search(root,number) == true) cout<<"Found\n";
-	else cout<<"Not Found\n";
+	//Code To Test the logic
+	//Creating an example tree
+
+	Node* root = NULL;
+	
+	root = Insert(root,6); 
+	root = Insert(root,17);
+	root = Insert(root,20); 
+	root = Insert(root,41); 
+	root = Insert(root,45); 
+	root = Insert(root,52);
+	
+	//Print Nodes in Level Order. 
+	LevelOrder(root);
 }
