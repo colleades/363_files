18c2c127127a8c1e631d6cf4e8361437fefee30f
Collin Eades <cjeades@tesla.cs.iupui.edu>
2018-10-01 00:20:16 -0400

    working program.  output formatting must change

--- bstKth.cpp
+++ bstKth.cpp
@@ -17,7 +17,6 @@ BstNode* GetNewNode(int data){
 	newNode->data = data;
 	newNode->left = NULL;
 	newNode->right = NULL;
-	cout<<"inside getnewnode";
 	return newNode;
 }
 
@@ -27,34 +26,51 @@ BstNode* Insert(BstNode* root, int data){
 	//if the root is empty, make data the root
 	if(root==NULL){
 		root = GetNewNode(data);
-		cout<<"added to root";
 	
 	}
 	//if data is less than root, put it on the left side
 	else if(data <= root->data){
 		root->left = Insert(root->left, data);
-		cout<<"put on the left side";
 	}
 	//if data is greater, right side
 	else{
 		root->right = Insert(root->right, data);
-		cout<<"put on the right side";
 	}
 	return root;
 }
 
-void PrintInOrder(BstNode* root, int kth){
+int PrintInOrder(BstNode* root, int kth, int counter){
+
+	//if there have been 15 node datas printed, return smallest element
+	//THIS COULD EASILY BE CHANGED TO BE DYNAMIC TO MORE THAN 15 
+	if (counter==15){
+		return kth;
+	}
 
 	if(root != NULL){
 
 		if (root->left != NULL){
 
-			PrintInOrder(root->left, kth);
+			//go to next node to the left 
+			PrintInOrder(root->left, kth, counter);
 		}
+
+		//print the nodes data
 		cout<<root->data<<" ";
+
+		//snag smallest when currently on it
+		if (counter==kth){
+
+			kth=root->data;
+		}
+	
+		//increase counter
+		counter=counter+1;
+		
 		if (root->right !=NULL){
 
-			PrintInOrder(root->right, kth);
+			//go to next node to the right
+			PrintInOrder(root->right, kth, counter);
 		}
 	}else{
 
@@ -64,15 +80,23 @@ void PrintInOrder(BstNode* root, int kth){
 
 int main(){
 
+
+	//variable for inputted array keys and K variables
+	int dataPassed, k;
+
+	//counter to pass to print to help calculate kth smallest
+	int counter=1;
+
 	//create empty tree (pointer to top of tree)
 	BstNode* root = NULL;
+
+	//promt for value of k
+	cout<<"\nHello. Please enter a value for K: ";
+	cin>>k;
 
 	//prompt user for data
 	cout<<"\nPlease enter (in order) each number in the array given for the assignemnt: ";
 	
-	//variable for inputted array keys and K
-	int dataPassed, k;
-
 	//data is inputted into tree
 	for (int i=1; i<=15; i++){
 
@@ -81,12 +105,16 @@ int main(){
 		root = Insert(root, dataPassed);
 	}
 
-	cout<<"\nTree has been constructed.  Please enter a value for K: ";
-	cin>>k;
+	//print tree
 	cout<<"\nThank you.  Printing tree...  ";
 
+
 	//call print function for tree, pass K
-	PrintInOrder(root, k);
+	k=PrintInOrder(root, k, counter);
+
+	//print kth smallest
+	cout<<"\nThe Kth smallest element is: "<<k;
+
 }
 
 
